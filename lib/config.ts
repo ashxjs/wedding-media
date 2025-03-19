5b617c2036fa8348cc6da26b0cdd7e76194e3477
ashxjs <hurunghee.alexis@gmail.com>
2025-03-19 18:01:34 +0100

    UPDATE: security

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,10 @@
+const isDev = process.env.NODE_ENV === "development";
+const keyFile = isDev
+  ? process.env.GOOGLE_CLOUD_KEY_FILE
+  : JSON.parse(process.env.GOOGLE_CLOUD_KEY_FILE ?? "");
+
 export const googleCloudConfig = {
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID ?? "",
-  keyFilename: process.env.GOOGLE_CLOUD_KEY_FILE ?? "",
+  keyFilename: keyFile,
   bucketName: process.env.GOOGLE_CLOUD_BUCKET_NAME ?? "",
 };
